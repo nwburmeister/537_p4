bfb5dfd7dce656560b216b2d3f888512d285cff2
Nikolas Burmeister <nikolas@rockhopper-05.cs.wisc.edu>
2019-10-14 21:29:09 -0500

    added user programs

--- xv6/userRR.c
+++ xv6/userRR.c
@@ -1,4 +1,27 @@
 //
 // Created by nikolas on 10/14/19.
 //
+#include "user.h"
 
+void roundRobin(int timeslice, int iterations, char *job, int jobcount){
+
+}
+
+
+void main(int argc, char *argv[]) {
+    if(argc != 5) {
+        // TODO: print error message
+        exit();
+    }
+
+    int timeslice = argv[1];
+    int iterations = argv[2];
+    char *job = malloc(sizeof(char) * (strlen(argv[3]) + 1));
+    strcpy(job, argv[3]);
+    int jobcount = argv[4];
+
+
+    roundRobin(timeslice, iterations, job, jobcount);
+    free(job);
+
+}
